73cb9e74b5e41e7bd3e3141fbf218ea92390e0f6
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-21 15:41:58 +0100

    Changed font-weight to bold

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex flex-col min-h-screen text-5xl font-extralight md:flex-row">
+    <div className="flex flex-col min-h-screen text-6xl font-bold md:text-7xl md:flex-row">
       <div className="flex items-end justify-center flex-1 p-4 text-white bg-black md:justify-end md:items-center">
         Hello
       </div>
