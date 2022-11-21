0bac6c02208b29fda7b15d32cb1cbc965194d741
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-21 15:22:27 +0100

    Designed page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return null;
+  return (
+    <div className="flex flex-col min-h-screen text-5xl font-extralight md:flex-row">
+      <div className="flex items-end justify-center flex-1 p-4 text-white bg-black md:justify-end md:items-center">
+        Hello
+      </div>
+      <div className="flex items-start justify-center flex-1 p-4 text-black bg-white md:justify-start md:items-center">
+        World
+      </div>
+    </div>
+  );
 }
 
 export default App;
