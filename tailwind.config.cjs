19d314c1d3de8283078aa73678461ae7e9d4fa33
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-21 15:21:37 +0100

    Configured montserrat font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+      spacing: {
+        2.5: "0.625rem",
+      },
+    },
   },
   plugins: [],
 };
