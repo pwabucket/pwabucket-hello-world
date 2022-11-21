d68fb163a33c113f9c4816da26bac0a8d1eaf143
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-21 16:03:46 +0100

    - Configured PWA manifest - Exposed dev server

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,34 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({ registerType: "autoUpdate", scope: "./" }), react()],
+  server: {
+    host: "0.0.0.0",
+  },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      scope: "./",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Hello World",
+        short_name: "HelloWorld",
+        description: "Hello World",
+        theme_color: "#000",
+        start_url: "./",
+        icons: [
+          {
+            src: "icon@192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon@512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+    react(),
+  ],
 });
